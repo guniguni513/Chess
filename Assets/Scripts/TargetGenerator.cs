ec55b828a28284967ad9e06f6248a751e10aa40a
guniguni <guniguni513@gmail.com>
2024-03-18 15:23:01 +0900

    Targetの出現span設定

--- Assets/Scripts/TargetGenerator.cs
+++ Assets/Scripts/TargetGenerator.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class TargetGenerator : MonoBehaviour
 {
     public GameObject target;
-    public int targets = 5;
+    float timer;
+    public float span = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < targets; i++)
-        {
-            float x = Random.Range(0.0f, 21.0f);
-            float y = Random.Range(0.0f, 3.0f);
-            Instantiate(
-            target,
-            new Vector3(x - 10, y, 10),
-            Quaternion.identity
-            );
-        }
+    }
+
+    IEnumerator abon()
+    {
+        //弾の連射制限に使える
+        yield return new WaitForSeconds(1f);
+    }
+
+    void CreateTarget()
+    {
+        float x = Random.Range(0.0f, 21.0f);
+        float y = Random.Range(0.0f, 3.0f);
+        Instantiate(
+        target,
+        new Vector3(x - 10, y, 10),
+        Quaternion.identity
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer >= span)
+        {
+            timer = 0;
+            CreateTarget();
+        }
 
     }
 }
